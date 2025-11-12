6aa5b40cdba77eed8a15e778fbd019ac63d89b0e
Index Webs <indexwebs@Rodrigos-MacBook-Air.local>
2025-11-11 21:14:28 -0300

    firebase arreglo

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -4,16 +4,30 @@ import 'firebase/firebase-auth'
 
 if (!firebase.apps.length) {
   const firebaseConfig = {
-    apiKey: "AIzaSyD9Wc3UBZb4YPZxLq52QiAEipk7en4v8Ns",
-    authDomain: "atscanner-1c6bc.firebaseapp.com",
-    projectId: "atscanner-1c6bc",
-    storageBucket: "atscanner-1c6bc.firebasestorage.app",
-    messagingSenderId: "860316812760",
-    appId: "1:860316812760:web:908920c0b911aacb881ead",
-    measurementId: "G-N4FS19K1E8"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_ADMIN_PROJECT_ID || process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '860316812760',
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID || undefined,
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig)
+
+  const missingKeys = Object.entries({
+    FIREBASE_API_KEY: firebaseConfig.apiKey,
+    FIREBASE_AUTH_DOMAIN: firebaseConfig.authDomain,
+    FIREBASE_PROJECT_ID: firebaseConfig.projectId,
+    FIREBASE_STORAGE_BUCKET: firebaseConfig.storageBucket,
+    FIREBASE_APP_ID: firebaseConfig.appId,
+  })
+    .filter(([, value]) => !value)
+    .map(([key]) => key);
+
+  if (missingKeys.length) {
+    throw new Error(`Faltan variables de entorno para Firebase: ${missingKeys.join(', ')}`);
+  }
+
+  firebase.initializeApp(firebaseConfig);
 }
 const db = firebase.firestore()
 export { db, firebase }
